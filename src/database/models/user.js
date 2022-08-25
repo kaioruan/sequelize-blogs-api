00c71ef159f5f66700e61253effc45f3279c90d0
kaioruan <kaio.ruan@souunit.com.br>
2022-08-25 16:12:54 -0300

    Feat: create model user

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    updatedAt: false,
+    underscored: true,
+    tableName: 'characters'
+  });
+
+  return User;
+};
