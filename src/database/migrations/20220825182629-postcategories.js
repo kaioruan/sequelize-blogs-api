75ade5e681197ad9ac013c77d41599bf8d5a2725
kaioruan <kaio.ruan@souunit.com.br>
2022-08-25 16:03:10 -0300

    Feat: create migration users/categories/blogposts/postcategories

---
+++ src/database/migrations/20220825182629-postcategories.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        foreignKey: true,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        foreignKey: true,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
