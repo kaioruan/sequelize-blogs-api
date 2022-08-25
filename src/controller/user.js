ac8d3befd0cde64367223f804b3cd67a2d363803
kaioruan <kaio.ruan@souunit.com.br>
2022-08-25 18:59:04 -0300

    Feat: create application post/user create user

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,17 +4,19 @@ const { User } = require('../database/models');
 const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 const ERROR_500 = 'Algo de errado não está certo';
 
-const loginController = {
+const userController = {
   getAll: async (req, res) => {
     try {
-      const { email } = req.body;
+      const { displayName, email, image } = req.body;
+      const passwordNew = req.body.password;
       const user = await User.findOne({ where: { email } });
-      if (!user || user.password !== req.body.password) {
-        return res.status(400).json({ message: 'Invalid fields' });
+      if (user) {
+        return res.status(409).json({ message: 'User already registered' });
       }
-      const { password, ...dataUser } = user;
+      const userCreate = await User.create({ displayName, email, password: passwordNew, image });
+      const { password, ...dataUser } = userCreate;
       const token = jwt.sign({ dataUser }, JWT_SECRET);
-      res.status(200).json({ token });
+      res.status(201).json({ token });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: ERROR_500 });
@@ -23,5 +25,5 @@ const loginController = {
 };
 
 module.exports = {
-  loginController,
+  userController,
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const app = require('./api');
 const login = require('./controller/login');
+const user = require('./controller/user');
 const { createValidation } = require('./middlewares/login');
+const { userValidation, nameValidation } = require('./middlewares/user');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -11,5 +13,6 @@ app.get('/', (_request, response) => {
   response.send();
 });
 app.post('/login', createValidation, login.loginController.getAll);
+app.post('/user', userValidation, nameValidation, user.userController.getAll);
 
 app.listen(port, () => console.log('ouvindo porta', port));
