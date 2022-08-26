ed15b4d3858264c6ce8dfc7309b823dd6ef44ac8
kaioruan <kaio.ruan@souunit.com.br>
2022-08-26 01:19:10 -0300

    Feat: create controller getUserById

--- src/controller/user.js
+++ src/controller/user.js
@@ -15,15 +15,29 @@ const userController = {
       return res.status(500).json({ message: ERROR_500 });
     }
 },
-tokenController: async (_req, res) => {
-  try {
-    const user = await User.userService.getAllUser();
-    return res.status(200).json(user);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: ERROR_500 });
-  }
-},
+  tokenController: async (_req, res) => {
+    try {
+      const user = await User.userService.getAllUser();
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: ERROR_500 });
+    }
+  },
+  getById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.userService.getById(id);
+      if (!user) {
+        return res.status(404).json({ message: 'User does not exist',
+        }); 
+      }
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: ERROR_500 });
+    }
+  },
 };
 
 module.exports = {
